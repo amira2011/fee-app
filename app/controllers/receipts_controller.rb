52c1d45beb639611a4b9f16270d7c48dc9cf2807
unknown <shaikh.abidali@gmail.com>
2020-07-18 16:26:00 +0530

    FEE

--- app/controllers/receipts_controller.rb
+++ app/controllers/receipts_controller.rb
@@ -1,3 +1,25 @@
 class ReceiptsController < ApplicationController
 
+
+def new
+  @receipt =Receipt.new
+  @student=Student.first
+  @receipt_no=Receipt.maximum(:id).to_i+1
+  @time=  ist Time.new
+  @time1= @time.strftime("%Y-%m-%d")
+  print(@time1)
+  @time= @time.year
+  @receipt_no= "EN".concat(@time.to_s,@receipt_no.to_s)
+
+end
+
+def create
+  redirect_to students_path
+end
+
+def ist(time)
+  time.in_time_zone(TZInfo::Timezone.get('Asia/Kolkata'))
+end
+
+
 end
